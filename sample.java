705fe42ad5236a085c929442850b39c02e2fce7e
VigneshVicky2001 <64652684+VigneshVicky2001@users.noreply.github.com>
2023-12-23 14:02:27 +0530

    changing b value

--- sample.java
+++ sample.java
@@ -4,5 +4,8 @@ public class sample{
     public static void main(String args[]){
         System.out.println("Hello World!");
         System.out.println("Hi");
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
     }
 }
